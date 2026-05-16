1873c026cd4c50b9631e67bfffb5fe2c0c9b28f4
Michael-Rafael-Andrade <michaelrafaeldeandrade@gmail.com>
2026-05-16 00:07:25 -0300

    Configurar as Rotas Iniciais
    Configurar as Rotas Iniciais

--- routes/rotasIndex.js
+++ routes/rotasIndex.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var router = express.Router();
+// Importa o controller
+const controllerIndex = require('../controller/controllerIndex');
 
+// Quando alguém acessar a raiz ('/'), executa a função tela_principal do controller
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Listagem de livros' });
-});
+// router.get('/', function(req, res, next) {
+//   res.render('index', { title: 'Listagem de livros' });
+// });
+router.get('/', controllerIndex.tela_principal);
 
 module.exports = router;
--- routes/rotasLivros.js
+++ routes/rotasLivros.js
@@ -1,10 +1,22 @@
 var express = require('express');
 var router = express.Router();
+// Importa o controller de livros
+const controllerLivros = require('../controller/controllerLivros');
 
 /* Rota livros */
-router.get('/', function(req, res, next){
-    res.render('livros', { title: 'Livros'} );
-});
+// router.get('/', function(req, res, next){
+//     res.render('livros', { title: 'Livros'} );
+// });
+
+// Rota para mostrar a tela de criação do livro
+router.get('/criar', controllerLivros.cria_get);
+
+// Rota para recebe os dados do formulário de criação
+router.post('/criar', controllerLivros.cria_post);
+
+// Rota para alterar o status
+router.get('/novo_status/:id/:novo_status', controllerLivros.altera_status);
+
 
 module.exports = router;
 

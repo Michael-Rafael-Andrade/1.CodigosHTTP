693f1b4dcb8b5d91a920a27cef6459cd456e9458
Michael-Rafael-Andrade <michaelrafaeldeandrade@gmail.com>
2026-05-17 00:06:35 -0300

    trabalhando coom cache
    trabalhando coom cache

--- controller/controllerSobre.js
+++ controller/controllerSobre.js
@@ -6,5 +6,10 @@ exports.cria_sobre = function(req, res){
         title: "Biblioteca comunitária"
     };
 
+    // Página pode ser armazenada em cache por 30 dias
+    res.set('Cache-control', 'public, max-age=2592000');
+
+    // Express gera Etag automaticamente
+
     return res.render('sobre', contexto);
 }
